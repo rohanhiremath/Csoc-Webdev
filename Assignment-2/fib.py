868711735d9779baff24435b0103963c64247bbe
Rohan Hiremath <rohanhiremath19@gmail.com>
2025-04-20 17:24:14 +0530

    Assignment 2

--- Assignment-2/fib.py
+++ Assignment-2/fib.py
@@ -1,8 +1,8 @@
-def print_fibonacci(n):
+def printfib(n):
     a, b = 0, 1
     for i in range(n):
         print(a, end=' ')
         a, b = b, a + b
 n = int(input("Input: "))
 print("Output:", end=' ')
-print_fibonacci(n)
+printfib(n)
